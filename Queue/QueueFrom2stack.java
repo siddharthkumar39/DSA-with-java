0695695614fe627f710f0d0a496585afc5a0075b
siddharthkumar <siddharthshr02@gmail.com>
2026-09-13 00:05:07 +0530

    hands on with some queue problems after internal exam

---
+++ Queue/QueueFrom2stack.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class QueueFrom2stack {
+
+    static class QueueB{
+        static Stack<Integer>s1=new Stack<>();
+            static Stack<Integer>s2=new Stack<>();
+
+            public static boolean isEmpty(){
+                return s1.isEmpty();
+            }
+            //add
+            public static void add(int data){
+                while(!s1.isEmpty()){
+                    s2.push(s1.pop());
+                }
+                s1.push(data);
+                while(!s2.isEmpty()){
+                    s1.push(s2.pop());
+                }
+
+            }
+
+            //remove
+            public static int remove(){
+                if(isEmpty()){
+                    System.out.println("Empty queue");
+                    return -1;
+                }
+                return s1.pop();
+            }
+            //peek
+            public static int peek(){
+                if(isEmpty()){
+                    System.out.println("empty queue");
+                    return -1;
+                }
+                return s1.peek();
+    }
+}
+    public static void main(String args[]){
+        QueueB q=new QueueB();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
+                
